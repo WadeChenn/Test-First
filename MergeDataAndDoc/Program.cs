2c4cfb4caeb4be9a2b71c7a3be274e673c5a12f9
Evil_Evince <821204616@qq.com>
2016-03-10 19:12:30 +0800

    input data

--- MergeDataAndDoc/Program.cs
+++ MergeDataAndDoc/Program.cs
@@ -13,6 +13,9 @@ namespace MergeDataAndDoc
         {
             string inputFileName = "defaultInput.txt";
             string outputFileName = "defaultOutput.txt";
+            string Data_ = "data.txt";
+            string Template_ = "template.txt";
+            string Result_ = "result.txt";
             if (args.Length == 2)
             {
                 inputFileName = args[0];
@@ -20,17 +23,27 @@ namespace MergeDataAndDoc
             }
             Console.WriteLine();
 
-            using (StreamReader inputFile = new StreamReader(inputFileName))
-            using(StreamWriter outputFile = new StreamWriter(outputFileName))
+            using (StreamReader DataFile = new StreamReader(Data_))
+            using (StreamReader TemplateFile = new StreamReader(Template_))
+            using (StreamWriter ResultFile = new StreamWriter(Result_))
             {
-                string line; //test
-                while((line = inputFile.ReadLine()) != null)
+                string line_Data; //test
+                string line_Temp;
+                while((line_Data = DataFile.ReadLine()) != null)
                 {
-                    string outputLine = "***" + line;
+                    string outputLine = "***" + line_Data;
                     Console.WriteLine("Write line: " + outputLine);
-                    outputFile.WriteLine(outputLine);
+                    //ResultFile.WriteLine(outputLine);
+                }
+                while ((line_Temp = TemplateFile.ReadLine()) != null)
+                {
+                    string outputLine2 = "***" + line_Temp;
+                    Console.WriteLine("Write line: " + outputLine2);
+                    //ResultFile.WriteLine(outputLine);
                 }
             }
+            //string str_ = "123陳燁煒";
+            //Console.WriteLine(str_);
         }
     }
 }
