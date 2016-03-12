63279000257d523b252f0358b29c21fee7a7b239
Evil_Evince <821204616@qq.com>
2016-03-12 18:43:58 +0800

    Ending

--- MergeDataAndDoc/Program.cs
+++ MergeDataAndDoc/Program.cs
@@ -12,8 +12,6 @@ namespace MergeDataAndDoc
     {
         static void Main(string[] args)
         {
-            //string inputFileName = "defaultInput.txt";
-            //string outputFileName = "defaultOutput.txt";
             string Data_ = "";
             string Template_ = "";
             string Result_ = "";
@@ -60,25 +58,16 @@ namespace MergeDataAndDoc
                         continue;
                     string outputLine =line_Data;
                     char[] Data_str = line_Data.ToCharArray();//字符串轉換為一個char型數組
-                    //Console.WriteLine("***"+outputLine);
-                    //for (int i = 0; i < Data_str.Length;i++ )
-                    //Console.Write(Data_str[i]);//測試輸出最後一個字符
 
                     //******提取變量******
                     年數[n,0] = Data_str[Data_str.Length - 2];
                     年數[n,1] = Data_str[Data_str.Length - 1];
-                    //Console.WriteLine(年數[n]);
                     for(int i=7;i>0;i--)
                     身份證字號[n,i] =Data_str[Data_str.Length-2-i];
-                    //Console.WriteLine(身份證字號[n]);
                     for(int i=0;i<Data_str.Length-9;i++)
                     中文姓名[n,i]=Data_str[i];
-                    //Console.WriteLine(中文姓名[n]);
-
                     n++;
                     Data_List = line_Data.ToList();
-
-                   //nsole.Write(Data_List[0]);//test
                 }
                 string str0;
                 n = 0;
@@ -87,11 +76,8 @@ namespace MergeDataAndDoc
                     while ((line_Temp = TemplateFile.ReadLine()) != null)//讀出template.txt
                     {
                         string outputLine2 = line_Temp;
-                        //Console.WriteLine(outputLine2);
                         Template_List = line_Temp.ToList();
-                        // Console.Write(Template_List[0]);//test
 
-                        // Console.Write(中文姓名.GetLength(0));
                         //*****處理字符串*****
                         Result_List = Template_List;//將Template字符串保存到Result字符串中.
                         for (int i = 0; i < Result_List.Count(); i++)
@@ -106,7 +92,6 @@ namespace MergeDataAndDoc
                                         {
                                             if (中文姓名[n, j] != '\0')
                                                 Result_List.Insert(i + j, 中文姓名[n, j]);//將姓名插入list中
-
                                         }
                                         break;
                                     case '身':
@@ -115,10 +100,7 @@ namespace MergeDataAndDoc
                                         {
                                             if (身份證字號[n, j] != '\0')
                                                 Result_List.Insert(i + j, 身份證字號[n, j]);//將身份證號碼插入list中
-
                                         }
-
-
                                         break;
                                     case '年':
                                         Result_List.RemoveRange(i, 5);
@@ -126,16 +108,11 @@ namespace MergeDataAndDoc
                                         {
                                             if (年數[0, j] != '\0')
                                                 Result_List.Insert(i + j, 年數[n, j]);//將年數插入到list中
-
                                         }
-
-
                                         break;
                                 }
-
                             }
                         }
-
                         for (int i = 0; i < Result_List.Count(); i++)
                         {
                             Console.Write(Result_List[i]);
@@ -143,20 +120,12 @@ namespace MergeDataAndDoc
                         }
                         Console.WriteLine();
                         ResultFile.WriteLine();
-                       
                     }
                     TemplateFile.BaseStream.Seek(0, SeekOrigin.Begin);
-                    //StreamReader.
                     n++;
                 }
             }
-            //string str_ = "123陳燁煒";
-            //Console.WriteLine(str_);
+
         }
     }
 }
-/*
- ${中文姓名}先生(身份證字號${身份證字號})為
-本校專任教師，聘期${年數}
-                         此聘
-                                   校長*/
