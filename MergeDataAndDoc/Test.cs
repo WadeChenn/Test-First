689c94067bfd68fd4c6871a0abc526bd95490bd8
Evil_Evince <821204616@qq.com>
2016-04-15 22:04:52 +0800

    Test_secont_end

--- MergeDataAndDoc/Test.cs
+++ MergeDataAndDoc/Test.cs
@@ -20,6 +20,15 @@ namespace MergeDataAndDoc
             Program.NewMethod(input_1, input_2, output);
             Assert.That( output.ToString(),Is.EqualTo("Tomc先生(身份證字號)4324321為本校專任教師，聘期2年\r\n"));
         }
+        [Test]
+        public void T2()
+        {
+            StringReader input_1 = new StringReader("中文姓名身份證字號年數\nEvice45679871年");
+            StringReader input_2 = new StringReader("${中文姓名}先生(身份證字號${身份證字號})為本校專任教師，聘期${年數}");
+            StringWriter output = new StringWriter();
+            Program.NewMethod(input_1, input_2, output);
+            Assert.That(output.ToString(), Is.EqualTo("Evice先生(身份證字號)7897654為本校專任教師，聘期1年\r\n"));
+        }
 
     }
 }
